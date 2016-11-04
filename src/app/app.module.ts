0b1837569de511cb35812f604e1b269bdebba60a
Ernie <ernest.homsy@parsons.com>
2016-11-04 16:37:37 -0400

    add Heroes and Dashboard pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+import {HeroService} from "./hero.service";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
-  providers: [],
+  providers: [HeroService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
